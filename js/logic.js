cbe93c16d409c8ec24eeec7fea74f68ef6015cec
peevo <peevo@yandex-team.ru>
2016-10-18 23:22:06 +0300

    Initial commit

---
+++ js/logic.js
@@ -0,0 +1,60 @@
+$(".cone_line").each(function() {
+    $(this).prop('numberOfChosenCones', 0);
+});
+
+$(".cone").on("click", function(event) {
+
+    var thisLine = $(this).parent();
+    numberOfCHosenCones = thisLine.prop('numberOfChosenCones');
+    if (!$(this).hasClass('chosen-cone')) {
+        $(this).addClass('chosen-cone');
+        thisLine.prop('numberOfChosenCones', numberOfCHosenCones + 1);
+
+    } else {
+        $(this).removeClass('chosen-cone');
+        thisLine.prop('numberOfChosenCones', numberOfCHosenCones - 1);
+    }
+
+    //deselect other lines and their cones if they are chosen
+    $(".cone_line.chosen-line").each(function() {
+        if (thisLine[0] != $(this)[0]) {
+            $(this).removeClass('chosen-line');
+            $(this).find('.cone').each(function() {
+                if ($(this).hasClass('chosen-cone')) {
+                    $(this).removeClass('chosen-cone');
+                    $(this).parent().prop('numberOfChosenCones',
+                        Math.max(0, numberOfCHosenCones - 1));
+                }
+            });
+        }
+        if ($(this).prop('numberOfChosenCones') == 0) {
+            if ($(this).hasClass('chosen-line')) {
+                $(this).removeClass('chosen-line')
+                $(".button-commit").css('display', 'none');
+            }
+        }
+    });
+
+    if (thisLine.prop('numberOfChosenCones') > 0) {
+        if (!thisLine.hasClass('chosen-line')) {
+            thisLine.addClass('chosen-line');
+            $(".button-commit").css('display', 'block');
+        }
+    }
+
+});
+
+$(".button-commit").on("click", function(event) {
+    $(".cone").each(function() {
+        if ($(this).hasClass('chosen-cone')) {
+            $(this).addClass('removed-cone');
+            thisLine = $(this).parent();
+            thisLine.prop('numberOfChosenCones', 0);
+            thisLine.removeClass('chosen-line');
+            $(".button-commit").css('display', 'none');
+        }
+    });
+    if ($(".removed-cone").length == 24) {
+        $('.cones').html("Проиграл! Иди за дровами");
+    };
+});
